5c8c04e1e677bf40c491fef2198295cd6d11686b
azhar <azharalamjawaid@gmail.com>
2023-10-09 12:57:48 -0700

    New Front-end

--- resources/views/inc/sidebar.blade.php
+++ resources/views/inc/sidebar.blade.php
@@ -52,8 +52,8 @@ foreach ($theme_colors as $key => $value2) {
 
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <li class="nav-item">
-          <a href="#" class="nav-link">
+      <li class="nav-item {{ request()->is('organization*', 'account_account=1*', 'buyers*', 'seller*', 'sales_officer*', 'warehouse*', 'zone*') ? 'menu-open' : '' }}">
+        <a href="#" class="nav-link {{ request()->is('organization*', 'account_account=1*', 'buyers*', 'seller*', 'sales_officer*', 'warehouse*', 'zone*') ? 'active' : '' }}">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
               Setup
@@ -61,25 +61,50 @@ foreach ($theme_colors as $key => $value2) {
             </p>
           </a>
           <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a href="/organization" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Organization</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/account_account=1" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Accounts</p>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a href="/users" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Users</p>
-              </a>
-            </li>
-          </ul>
+    <li class="nav-item">
+        <a href="/organization" class="nav-link{{ request()->is('organization*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Organization</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/account_account=1" class="nav-link{{ request()->is('account_account=1*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Accounts</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/buyers" class="nav-link{{ request()->is('buyers*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Customers</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/seller" class="nav-link{{ request()->is('seller*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Supplier</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/sales_officer" class="nav-link{{ request()->is('sales_officer*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Sales Officer</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/warehouse" class="nav-link{{ request()->is('warehouse*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Warehouse</p>
+        </a>
+    </li>
+    <li class="nav-item">
+        <a href="/zone" class="nav-link{{ request()->is('zone*') ? ' active' : '' }}">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Zone</p>
+        </a>
+    </li>
+</ul>
+
         </li>
       </ul>
     </nav>
