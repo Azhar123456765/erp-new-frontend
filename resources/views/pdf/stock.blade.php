bd7060cbb2374576bce22f6b9de47b77aa6caba1
azhar <azharalamjawaid@gmail.com>
2023-12-13 06:44:07 -0800

    TESTING

--- resources/views/pdf/stock.blade.php
+++ resources/views/pdf/stock.blade.php
@@ -90,11 +90,14 @@ $type = session()->get('Data')['type'] ?? null;
     </thead>
     <tbody>
         <?php
+                           $pqty = App\Models\purchase_invoice::where('item', $row->product->product_id)->sum('pur_qty');
+                           $sqty = App\Models\sell_invoice::where('item', $row->product->product_id)->sum('sale_qty');
+                           $avail_qty = $pqty-$sqty;
         $pi = session()->get('Data')['pi'];
         foreach ($pi as $key => $row) {
             $si = session()->get('Data')['si'];
         ?>
-                <tr style="text-align: center; {{$row->product->opening_quantity <= 0 ? 'color: red;' : ''}} {{$row->product->opening_quantity == null ? 'color: black;' : ''}}">
+                <tr style="text-align: center; {{$avail_qty <= 0 ? 'color: red;' : ''}} {{$row->product->opening_quantity == null ? 'color: yellow;' : ''}}">
                     <td>
                         <span><?php echo $row->product->product_name ?></span>
                     </td>
@@ -114,11 +117,8 @@ $type = session()->get('Data')['type'] ?? null;
                         <span><?php $qty = App\Models\sell_invoice::where('item', $row->product->product_id)->sum('return_qty'); echo $qty;?></span>
                     </td>
                     <td>
-                        <span><?php  
-                            $pqty = App\Models\purchase_invoice::where('item', $row->product->product_id)->sum('pur_qty');
-                            $sqty = App\Models\sell_invoice::where('item', $row->product->product_id)->sum('sale_qty');
-
-                            echo $pqty-$sqty;
+                        <span><?php   
+                            echo $avail_qty;
                         ?></span>
                     </td>
             <?php
